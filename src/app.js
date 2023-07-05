1447d5a7dddc0f68e2eb71b51a46d264ad1554a2
Imtiaz Masrur <im_oli106@yahoo.com>
2023-07-05 16:55:26 +0600

    add env

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 'use strict';
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.NODE_ENV | 3000;
 
 app.listen(port, () => {
     console.info(`API server started on PORT: ${port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send(`Hello World`);
+});
+
+app.get('/port', (req, res) => {
+    res.send(`Server PORT ${process.env.DB_DIALECT}`);
 });
